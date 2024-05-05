f3c2a2dff775789d6a9ec1f2c77bab803cea7040
Luiz Ricardo Junior <166957887+LuizRicJunior@users.noreply.github.com>
2024-05-05 18:24:28 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia(numero: 848152, modelo: "Nokia", imei:"9838938", memoria: 128);
+Console.WriteLine("Smartphone nokia..");
+nokia.Ligar();
+nokia.InstalarAplicativo("telegram");
+Console.WriteLine("");
+
+
+Console.WriteLine("Smartphone Iphone..");
+Iphone iphone = new Iphone(numero: 85111, modelo: "Iphone", imei:"55115181", memoria: 64);
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Chromme");
+Console.WriteLine("");
